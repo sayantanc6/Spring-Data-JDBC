5c7c2d8ab2c89e71c3314400f7572239f1f05cc9
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2021-05-28 17:35:11 +0530

    Update MyjdbcRepository.java
    na

--- JDBC-Sample/src/main/java/dummy/repo/MyjdbcRepository.java
+++ JDBC-Sample/src/main/java/dummy/repo/MyjdbcRepository.java
@@ -1,11 +1,20 @@
 package dummy.repo;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import dummy.entity.Book;
 
 @Repository
-public interface MyjdbcRepository extends CrudRepository<Book, Integer> {		
+@Transactional
+public interface MyjdbcRepository extends CrudRepository<Book, Integer> {	
+	
+	@Modifying
+	@Query("update book b set b.author =:author, b.title =:title where b.isbn =:isbn")
+	void updateBook(@Param("author") String author,@Param("title") String title, @Param("isbn") int isbn);
 	
 }
