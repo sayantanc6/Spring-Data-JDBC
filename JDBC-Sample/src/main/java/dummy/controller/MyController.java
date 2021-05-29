bbb4ee0da65adc7e66d3e8c95124941e44e65793
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2021-05-29 19:05:09 +0530

    Update MyController.java
    na

--- JDBC-Sample/src/main/java/dummy/controller/MyController.java
+++ JDBC-Sample/src/main/java/dummy/controller/MyController.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,16 +16,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import dummy.entity.Author;
 import dummy.entity.Book;
+import dummy.repo.AuthorRepository;
+import dummy.repo.BookRepository;
 import dummy.repo.MyBookDao;
-import dummy.repo.MyjdbcRepository;
 
 
 @RestController
 public class MyController {
 	
 	@Autowired
-	MyjdbcRepository repo;
+	AuthorRepository authorrepo;
+	
+	@Autowired
+	BookRepository repo;
 	
 	List<Book> booklist;
 	
@@ -66,10 +73,10 @@ public class MyController {
 			headers = "Accept=application/json")
 	public int[] batchinsert() {
 		booklist = new ArrayList<Book>();
-		booklist.add(new Book("author1",103,"title1",50.05));
-		booklist.add(new Book("author2",104,"title2",50.10));
-		booklist.add(new Book("author3",105,"title3",50.15));
-		booklist.add(new Book("author4",106,"title4",50.20));
+		booklist.add(new Book("author1",103,"title1",50.05,null));
+		booklist.add(new Book("author2",104,"title2",50.10,null));
+		booklist.add(new Book("author3",105,"title3",50.15,null));
+		booklist.add(new Book("author4",106,"title4",50.20,null));
 		 return dao.batchInsert(booklist);
 	}
 
@@ -78,10 +85,10 @@ public class MyController {
 			headers = "Accept=application/json")
 	public Iterable<Book> batchUpdate() {
 		booklist = new ArrayList<Book>();
-		booklist.add(new Book("author",103,"title",50.05));
-		booklist.add(new Book("author",104,"title",50.10));
-		booklist.add(new Book("author",105,"title",50.15));
-		booklist.add(new Book("author",106,"title",50.20));
+		booklist.add(new Book("author",103,"title",50.05,null));
+		booklist.add(new Book("author",104,"title",50.10,null));
+		booklist.add(new Book("author",105,"title",50.15,null));
+		booklist.add(new Book("author",106,"title",50.20,null));
 		return repo.saveAll(booklist);
 	}
 
@@ -90,10 +97,25 @@ public class MyController {
 			headers = "Accept=application/json")
 	public void batchdelete() {
 		booklist = new ArrayList<Book>();
-		booklist.add(new Book("author",103,"title",50.05));
-		booklist.add(new Book("author",104,"title",50.10));
-		booklist.add(new Book("author",105,"title",50.15));
-		booklist.add(new Book("author",106,"title",50.20));
+		booklist.add(new Book("author",103,"title",50.05,null));
+		booklist.add(new Book("author",104,"title",50.10,null));
+		booklist.add(new Book("author",105,"title",50.15,null));
+		booklist.add(new Book("author",106,"title",50.20,null));
 		repo.deleteAll(booklist);
+	}
+	
+	@GetMapping(value = "/book",
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			headers = "Accept=application/json")
+	public List<Book> findBooksauthorID(@RequestParam("authorID") int authorID) {
+		return repo.findByAuthorID(authorID);
+	}
+	
+	// https://stackoverflow.com/questions/35992342/how-to-send-special-characters-in-request-parameters-in-spring
+	@GetMapping(value = "/author",
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			headers = "Accept=application/json")
+	public List<Author> findauthors(@RequestParam("exp") String exp) {
+		return authorrepo.findByFullnameLike(exp);
 	}
 }
